2647e536ba13c612ba255bce19c859724b540c22
Udaya M <annmariyammm85@gmail.com>
2026-03-25 17:44:50 +0530

    Update day 6 file

--- day 5/Leetcode1108.java
+++
@@ -1,5 +0,0 @@
-public class Leetcode1108 {
-    public String defangIPaddr(String address) {
-        return address.replace(".","[.]");
-    }
-}
--- day 5/Leetcode28.java
+++
@@ -1,8 +0,0 @@
-public class Leetcode28 {
-    public int strStr(String haystack, String needle) {
-        for(int i=0;i<=haystack.length()-needle.length();i++){
-            if(haystack.substring(i,i+needle.length()).equals(needle)) return i;
-        }
-        return -1;
-    }
-}
