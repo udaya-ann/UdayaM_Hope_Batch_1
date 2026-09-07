974cbf8e47f7e91198ce2228e6b206d1951cc817
udaya-ann <annmariyammm85@gmail.com>
2026-09-07 11:14:18 +0530

    Time: 0 ms (100%), Space: 44.5 MB (92.21%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>=target){
+                return i;
+            }
+        }
+        if(nums[nums.length-1]<target){
+            return nums.length;
+        }
+        return 0;
+    }
+}
