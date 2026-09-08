d593a89681bb5a031ebceefb94b536906239dac7
udaya-ann <annmariyammm85@gmail.com>
2026-09-08 12:09:22 +0530

    Time: 0 ms (100%), Space: 80.2 MB (76.92%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
@@ -11,7 +11,7 @@ class Solution {
             else if(arr[mid]<arr[mid+1]){
                 left=mid+1;
             }else{
-                right=mid;
+                right=mid-1;
             }
         }
         return 0;
